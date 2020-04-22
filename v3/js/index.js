f6d62a9706c72e00b3ee506a7b23f88144f95858
xiaona07 <queena5@163.comm>
2020-04-22 23:01:57 +0800

    实现了数组方法监测

--- v3/js/index.js
+++ v3/js/index.js
@@ -1,18 +1,26 @@
 function SelfVue (options) {
-    var self = this;
+    let self = this;
     this.data = options.data;
     this.methods = options.methods;
     this.watch = options.watch;
-
     Object.keys(this.data).forEach(function(key) {
         self.proxyKeys(key);
     });
+    Object.keys(options.computed).forEach((key) => {
+        self.proxyComputed(key,options.computed[key])
+    })
     observe(this.data);
+    Object.keys(options.watch).forEach((key) => {
+        self.$watch(key,options.watch[key])
+    })
     new Compile(options.el, this);
     options.mounted.call(this); // 所有事情处理好后执行mounted函数
 }
 
 SelfVue.prototype = {
+    $watch: function(key, cb) {
+        new Watcher(this, key, cb);
+    },
     proxyKeys: function (key) {
         var self = this;
         Object.defineProperty(this, key, {
@@ -23,6 +31,14 @@ SelfVue.prototype = {
             },
             set: function setter (newVal) {
                 self.data[key] = newVal;
+            }
+        });
+    },
+    proxyComputed: function (key,fun) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            get: fun,
+            set: function () {
             }
         });
     }
